479da73299d60e215ef345c16258ccb53238961e
FIRST_NAME LAST_NAME <mohamedTaha12@github.com>
2023-10-05 08:36:53 +0300

    uploade

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SharedModule,
+    OverlayPanelModule,
+    ButtonModule
+  ], exports: [
+    HomeComponent
+  ]
+})
+export class HomeModule { }
