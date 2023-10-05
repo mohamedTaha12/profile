a210e2a2290342d86ec75c522195552be3ce5580
FIRST_NAME LAST_NAME <mohamedTaha12@github.com>
2023-10-05 20:32:25 +0300

    footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { InformationComponent } from './information/information.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    InformationComponent
+    InformationComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ], exports: [
     NavbarComponent,
-    InformationComponent
+    InformationComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
